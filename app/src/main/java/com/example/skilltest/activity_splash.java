a3ca476125e07310ac40af8601f8a4beeec481f5
AvoMelkonian <melkonianavo7@gmail.com>
2022-12-11 22:46:14 +0300

    Changes and adjustments 1

---
+++ app/src/main/java/com/example/skilltest/activity_splash.java
@@ -0,0 +1,20 @@
+package com.example.skilltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import java.util.Objects;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_splash);
+    }
+}
