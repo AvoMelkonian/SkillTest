32614c881f075d139a62fbd180a6de5bd63b57fa
AvoMelkonian <melkonianavo7@gmail.com>
2022-12-12 11:22:19 +0300

    Changes and adjustments 1

--- app/src/main/java/com/example/skilltest/activity_main.java
+++ app/src/main/java/com/example/skilltest/activity_main.java
@@ -13,7 +13,7 @@ public class activity_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
-        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
+        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar without causing null pointer
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
